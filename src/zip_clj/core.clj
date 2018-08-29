b57b559f87a23d7e2aa1699c0781ff8de51d3b19
Paul Adams <paadams@nmu.edu>
2018-08-29 00:29:36 -0500

    initial commit

---
+++ src/zip_clj/core.clj
@@ -0,0 +1,9 @@
+(ns zip-clj.core)
+
+(defn sum [x y]
+  (+ x y))
+
+(defn -main
+  "I don't do a whole lot."
+  []
+  (println (sum 10 10)))
---
+++ test/zip_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns zip-clj.core-test
+  (:require [clojure.test :refer :all]
+            [zip-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
